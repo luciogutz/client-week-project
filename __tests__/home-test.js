c8e49f14b48a79bcf0f04297fcc4f8a4a96ab626
Jeremy Ward <jeremy2929@twc.com>
2017-01-11 10:04:44 -0600

    Set up some JEST testing for home.js

--- __tests__/home-test.js
+++ __tests__/home-test.js
@@ -1,3 +1,36 @@
 import React from "react"
 import TestUtils from "react-addons-test-utils"
-import Home from "../js/home.js"
+import Homefile from "../js/home.js"
+
+describe("Posting", () => {
+  let postComponent = {}
+
+  beforeEach(()=>{
+    // This is NOT a reference to the element in the page...
+    //   it IS a reference to the component code
+    postComponent = TestUtils.renderIntoDocument(<Homefile />)
+  })
+
+  it("Should display 'Author Name'", ()=>{
+    const currentAuthorNameElement = TestUtils.findRenderedDOMComponentWithClass(
+      postComponent,
+      "nameAuthor"
+    )
+    expect(currentAuthorNameElement.textContent).toBe("Author Name")
+  })
+  it("Should display post date'", ()=>{
+    const currentPostDateElement = TestUtils.findRenderedDOMComponentWithClass(
+      postComponent,
+      "postDate"
+    )
+    expect(currentPostDateElement.textContent).toBe(" 04/21/2015 ")
+  })
+  it("Should display post title'", ()=>{
+    const currentTitleDateElement = TestUtils.findRenderedDOMComponentWithClass(
+      postComponent,
+      "postTitle"
+    )
+    expect(currentTitleDateElement.textContent).toBe("A New Year")
+  })
+
+})
