831212459ffd30389ad78db39406703cd0c374fe
Jeremy Ward <jeremy2929@twc.com>
2017-01-12 10:18:59 -0600

    HTML tweaded to handle both Home and Create, added code to add new posting to object

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './header'
 import Footer from './footer'
-import CreatePost from './createPost'
+//import CreatePost from './createPost'
 
 export default React.createClass({
   getInitialState() {
@@ -13,7 +13,6 @@ export default React.createClass({
           postDate: "04/21/2015",
           postTitle: "A New Year",
           postText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-          id: 0,
         },
         {
           postAuthor: "New Guy",
@@ -21,7 +20,6 @@ export default React.createClass({
           postDate: "05/11/2015",
           postTitle: "A New Day",
           postText: "In American folklore, Bigfoot (also known as Sasquatch) is a simian-like creature[2] that is said to inhabit forests, mainly in the Pacific Northwest. Bigfoot is usually described as a large, hairy, bipedal humanoid. The term sasquatch is an Anglicized derivative of the Halkomelem word sásq'ets",
-          id: 1,
         },
         {
           postAuthor: "Different Guy",
@@ -29,15 +27,13 @@ export default React.createClass({
           postDate: "06/13/2015",
           postTitle: "A New Week",
           postText: "An ocean is a body of saline water that composes much of a planet's hydrosphere.[2] On Earth, an ocean is one of the major conventional divisions of the World Ocean, which covers almost 71% of its surface.",
-          id: 2,
         },
         {
           postAuthor: "Different Guy",
           postAuthorIcon: "styles/authorIcon3.png",
           postDate: "06/13/2016",
           postTitle: "A New Month",
-          postText: "Lorem",
-          id: 3,
+          postText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecatm",
         },
         {
           postAuthor: "Author Name",
@@ -45,21 +41,120 @@ export default React.createClass({
           postDate: "04/21/2015",
           postTitle: "A New Year",
           postText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-          id: 0,
+        },
+        {
+          postAuthor: "Author Name",
+          postAuthorIcon: "styles/authorIcon1.png",
+          postDate: "04/21/2015",
+          postTitle: "A New Year",
+          postText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        },
+        {
+          postAuthor: "New Guy",
+          postAuthorIcon: "styles/authorIcon2.png",
+          postDate: "05/11/2015",
+          postTitle: "A New Day",
+          postText: "In American folklore, Bigfoot (also known as Sasquatch) is a simian-like creature[2] that is said to inhabit forests, mainly in the Pacific Northwest. Bigfoot is usually described as a large, hairy, bipedal humanoid. The term sasquatch is an Anglicized derivative of the Halkomelem word sásq'ets",
+        },
+        {
+          postAuthor: "Different Guy",
+          postAuthorIcon: "styles/authorIcon3.png",
+          postDate: "06/13/2015",
+          postTitle: "A New Week",
+          postText: "An ocean is a body of saline water that composes much of a planet's hydrosphere.[2] On Earth, an ocean is one of the major conventional divisions of the World Ocean, which covers almost 71% of its surface.",
+        },
+        {
+          postAuthor: "Different Guy",
+          postAuthorIcon: "styles/authorIcon3.png",
+          postDate: "06/13/2016",
+          postTitle: "A New Month",
+          postText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat",
+        },
+        {
+          postAuthor: "Author Name",
+          postAuthorIcon: "styles/authorIcon1.png",
+          postDate: "04/21/2015",
+          postTitle: "A New Year",
+          postText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        },
+        {
+          postAuthor: "Author Name",
+          postAuthorIcon: "styles/authorIcon1.png",
+          postDate: "04/21/2015",
+          postTitle: "A New Year",
+          postText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        },
+        {
+          postAuthor: "New Guy",
+          postAuthorIcon: "styles/authorIcon2.png",
+          postDate: "05/11/2015",
+          postTitle: "A New Day",
+          postText: "In American folklore, Bigfoot (also known as Sasquatch) is a simian-like creature[2] that is said to inhabit forests, mainly in the Pacific Northwest. Bigfoot is usually described as a large, hairy, bipedal humanoid. The term sasquatch is an Anglicized derivative of the Halkomelem word sásq'ets",
+        },
+        {
+          postAuthor: "Different Guy",
+          postAuthorIcon: "styles/authorIcon3.png",
+          postDate: "06/13/2015",
+          postTitle: "A New Week",
+          postText: "An ocean is a body of saline water that composes much of a planet's hydrosphere.[2] On Earth, an ocean is one of the major conventional divisions of the World Ocean, which covers almost 71% of its surface.",
+        },
+        {
+          postAuthor: "Different Guy",
+          postAuthorIcon: "styles/authorIcon3.png",
+          postDate: "06/13/2016",
+          postTitle: "A New Month",
+          postText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecatm",
+        },
+        {
+          postAuthor: "Author Name",
+          postAuthorIcon: "styles/authorIcon1.png",
+          postDate: "04/21/2015",
+          postTitle: "A New Year",
+          postText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        },
+        {
+          postAuthor: "Author Name",
+          postAuthorIcon: "styles/authorIcon1.png",
+          postDate: "04/21/2015",
+          postTitle: "A New Year",
+          postText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        },
+        {
+          postAuthor: "New Guy",
+          postAuthorIcon: "styles/authorIcon2.png",
+          postDate: "05/11/2015",
+          postTitle: "A New Day",
+          postText: "In American folklore, Bigfoot (also known as Sasquatch) is a simian-like creature[2] that is said to inhabit forests, mainly in the Pacific Northwest. Bigfoot is usually described as a large, hairy, bipedal humanoid. The term sasquatch is an Anglicized derivative of the Halkomelem word sásq'ets",
+        },
+        {
+          postAuthor: "Different Guy",
+          postAuthorIcon: "styles/authorIcon3.png",
+          postDate: "06/13/2015",
+          postTitle: "A New Week",
+          postText: "An ocean is a body of saline water that composes much of a planet's hydrosphere.[2] On Earth, an ocean is one of the major conventional divisions of the World Ocean, which covers almost 71% of its surface.",
+        },
+        {
+          postAuthor: "Different Guy",
+          postAuthorIcon: "styles/authorIcon3.png",
+          postDate: "06/13/2016",
+          postTitle: "A New Month",
+          postText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat",
+        },
+        {
+          postAuthor: "Author Name",
+          postAuthorIcon: "styles/authorIcon1.png",
+          postDate: "04/21/2015",
+          postTitle: "A New Year",
+          postText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         },
       ]
     }
   },
 
-  onClickSubmit(e) {
-    e.preventDefault();
-    let titleText = this.refs.title.value
-    let textPost = this.refs.textPost.value
-
-  },
 
   render() {
     return (
+      <div>
         <section>
           <Header />
           <h1 className="postHistoryTitle">List of Posts</h1>
@@ -75,10 +170,7 @@ export default React.createClass({
                    })
                    }
           </ul>
-          <CreatePost />
-          <Footer />
         </section>
-
         <section className="createPost__section">
           <button className="homeButton">Home</button>
           <h1 className="postAreaTitle">Create Your Spurs Post Here</h1>
@@ -88,6 +180,9 @@ export default React.createClass({
             <submit className="submitPost" onClick={ this.onClickSubmit } >SUBMIT</submit>
           </form>
         </section>
+        <Footer />
+      </div>
+
     )
   }
 })
