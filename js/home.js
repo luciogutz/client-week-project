de1c7e402ebf7a624dadc3033b3e8eef883a9a18
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-01-13 14:16:06 -0600

    Finished inserting values to render in details page

--- js/home.js
+++ js/home.js
@@ -200,9 +200,13 @@ export default React.createClass({
       var showTitle = this.state.posts[indexOfPostTitle].postTitle
       var showText = this.state.posts[indexOfPostTitle].postText
       var showImg = this.state.posts[indexOfPostTitle].photoUrl
+      var showAuthor = this.state.posts[indexOfPostTitle].postAuthor
+      var showDate = this.state.posts [indexOfPostTitle].postDate
       this.setState({showTitle})
       this.setState({showText})
       this.setState({showImg})
+      this.setState({showAuthor})
+      this.setState({showDate})
     },
     onClickSubmit(e) {
       e.preventDefault()
@@ -267,13 +271,16 @@ export default React.createClass({
           <div className="detail_post_wrapper">
          <div className="detail_post_info">
            <button className="detail_post_back_button"type="button" name="button">Back</button>
-           {console.log(this.state.indexOfPostTitle)}
            <h1 className="post_info"> {this.state.showTitle} </h1>
           </div>
           <div className="detail_post_img">
             <img className="" src={this.state.showImg} alt=""></img>
           </div>
-          <p className="detail_post_content"> {this.state.showText} </p>
+          <div className="detail_post_content">
+            <p> {this.state.showDate} </p>
+            <p> By: {this.state.showAuthor}</p>
+            <p> {this.state.showText} </p>
+          </div>
           </div>
         </section>
        <Footer />
