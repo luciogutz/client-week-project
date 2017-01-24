14fbd80e02606b39bee8f26b7c678b44aeb36e63
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-01-24 16:04:18 -0600

    /

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
   entry: "./js/index.js",
   devtool: "source-map",
   output: {
-      path: "public/dist",
+      path: "/public/dist",
       filename: "bundle.js"
   },
 module: {
